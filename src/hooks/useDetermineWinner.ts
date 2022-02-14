a6a547b2ff517bb6aca10b7e274dda5f6168b41d
Yana Kolodyazhnaya <yana.kolodyazhnaya19@gmail.com>
2022-02-14 15:24:43 +0300

    add win logic

---
+++ src/hooks/useDetermineWinner.ts
@@ -0,0 +1,15 @@
+const isContained = (combination: number[], searchCombination: number[]) => {
+    let res = []
+    for (let i = 0; i < searchCombination.length; i++) {
+        !combination.includes(searchCombination[i]) ? res.push(false) : res.push(true)
+    }
+    return !res.includes(false)
+}
+
+export const useDetermineWinner = (combination: number[], winnerCombination: number[][]) => {
+    let res = []
+    for (let i = 0; i < winnerCombination.length; i++) {
+        isContained(combination, winnerCombination[i]) ? res.push(true) : res.push(false)
+    }
+    return res.includes(true)
+}
--- src/models/field/index.ts
+++ src/models/field/index.ts
@@ -1,4 +1,4 @@
-import {createEvent, createStore} from 'effector'
+import {combine, createEvent, createStore} from 'effector'
 import {FieldI} from "../../interfaces/field"
 
 export const changeField = createEvent<FieldI[]>('change-field')
@@ -8,3 +8,18 @@ export const markField = createEvent<number>('mark-field')
 export const $fieldModel = createStore<FieldI[]>(
     Array(9).fill({}).map((field, index) => ({...field, id: index}))
 )
+
+export const $winnerFields = createStore<number[][]>([
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+])
+
+export const $crossWinCombination = combine($fieldModel, fields => fields.filter(item => item.value === "cross").map(item => item.id))
+export const $zeroWinCombination = combine($fieldModel, fields => fields.filter(item => item.value === "zero").map(item => item.id))
+
